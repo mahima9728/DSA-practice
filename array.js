c1a059ae99859d185b357364f632fdaae8f7b084
mahima9728 <mahimarawat979@gmail.com>
2025-05-21 12:19:36 +0530

    adding

---
+++ array.js
@@ -0,0 +1,6 @@
+console.log("Working...");
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Reverse only the numbers at even indexes from an array.
+// Input: [1, 2, 3, 4, 5, 6] → Output: [5, 2, 3, 4, 1, 6]
